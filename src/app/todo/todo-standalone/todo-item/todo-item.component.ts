331651f33de960487143375b26be4b212429ecea
Ternovskiy <ilyafill@mail.ru>
2024-03-20 11:00:51 +0500

    Directive composition

--- src/app/todo/todo-standalone/todo-item/todo-item.component.ts
+++ src/app/todo/todo-standalone/todo-item/todo-item.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { Status, Todo } from '../../todo.service';
 import { NgSwitch, NgSwitchCase } from '@angular/common';
+import { ShadowDirective } from '../../shadow.directive';
 
 @Component({
-    selector: 'app-todo-item',
-    templateUrl: './todo-item.component.html',
-    styleUrl: './todo-item.component.scss',
-    standalone: true,
-    imports: [NgSwitch, NgSwitchCase],
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss',
+  standalone: true,
+  imports: [NgSwitch, NgSwitchCase],
+  hostDirectives: [ShadowDirective],
 })
 export class TodoItemComponent {
   protected readonly Status = Status;
