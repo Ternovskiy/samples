18a37abb114c2e20573031ca21d905e678b24c9e
Ternovskiy <ilyafill@mail.ru>
2024-03-20 10:12:42 +0500

    Remove unnecessary NgModule classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { TodoModuleModule2 } from './todo/todo-standalone/todo-module.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, TodoModuleModule2],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
